50d4147977bdd9c9bcfada91516e955063324035
sotberd <sotirios.berdes@squaredev.io>
2021-12-16 19:09:07 +0200

    add sites page functionality

--- sqd-cloud-dashboard/pages/sites.js
+++ sqd-cloud-dashboard/pages/sites.js
@@ -1,24 +1,46 @@
-import React from "react";
-import { Box, Button, Container, Typography, Grid } from "@mui/material";
-import SiteContainer from "../components/SiteContainer";
-import { Layout } from "../components/layout";
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Container, Typography, Grid } from '@mui/material';
+import SiteContainer from '../components/SiteContainer';
+import { Layout } from '../components/layout';
+import {
+  useDeleteStackByNameMutation,
+  useGetStacksByTypeMutation,
+} from '../app/services/api';
 
 export default function Sites() {
+  const [getSites, { isLoading }] = useGetStacksByTypeMutation();
+  const [deleteSite, { isDeleting }] = useDeleteStackByNameMutation();
+  const [sites, setSites] = useState([]);
+
+  const handleDeleteSiteClick = async (name) => {
+    const deleteAppResponse = await deleteSite(name).unwrap();
+    if (deleteAppResponse) {
+      setSites((app) => app.filter((a) => a.stackName !== name));
+    }
+  };
+
+  useEffect(async () => {
+    const getSitesResponse = await getSites(['site']).unwrap();
+    if (getSitesResponse) {
+      setSites(getSitesResponse);
+    }
+  }, []);
+
   return (
     <Layout>
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "space-between",
+          display: 'flex',
+          justifyContent: 'space-between',
         }}
       >
         <Typography
           variant="h5"
           sx={{
             flex: 1,
-            marginRight: "24px",
-            paddingBottom: "10px",
-            borderBottom: "1px solid #A4ACC4",
+            marginRight: '24px',
+            paddingBottom: '10px',
+            borderBottom: '1px solid #A4ACC4',
           }}
         >
           Sites
@@ -29,12 +51,17 @@ export default function Sites() {
         container
         spacing={{ xs: 2, md: 3 }}
         sx={{
-          padding: "38px 0",
+          padding: '38px 0',
         }}
       >
-        <SiteContainer />
-        <SiteContainer />
-        <SiteContainer />
+        {sites &&
+          sites.map((site, i) => (
+            <SiteContainer
+              key={i}
+              stack={site}
+              onDelete={handleDeleteSiteClick}
+            />
+          ))}
       </Grid>
     </Layout>
   );
