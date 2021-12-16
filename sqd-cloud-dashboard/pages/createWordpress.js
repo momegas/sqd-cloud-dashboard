42267795a7304893537dc4927fef8fcdd039a08e
anacolell <anacolell8@gmail.com>
2021-12-16 16:21:44 +0200

    Added tabs for create apps

--- sqd-cloud-dashboard/pages/createWordpress.js
+++ sqd-cloud-dashboard/pages/createWordpress.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { Container, Typography, TextField, Button } from '@mui/material';
-import { Layout } from '../components/layout';
-import { Box } from '@mui/system';
-import Image from 'next/image';
-import { useCreateWPMutation } from '../app/services/api';
-import { useRouter } from 'next/router';
+import React, { useState } from "react";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Layout } from "../components/layout";
+import { Box } from "@mui/system";
+import Image from "next/image";
+import { useCreateWPMutation } from "../app/services/api";
+import { useRouter } from "next/router";
 
 export default function createWordpress() {
   const router = useRouter();
@@ -13,102 +20,113 @@ export default function createWordpress() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const name = e.target.wordpressname.value;
-    const createWPResponse = await createWp({ name, type: 'wp' }).unwrap();
+    const createWPResponse = await createWp(name).unwrap();
     if (createWPResponse) {
-      router.push('/wordpress');
+      router.push("/wordpress");
     }
   };
 
+  //tabs
+  const [value, setValue] = React.useState(0);
+  const handleTabs = (e, val) => {
+    setValue(val);
+  };
+
+  function TabPanel(props) {
+    const { children, value, index } = props;
+    return <div>{value === index && <div> {children}</div>}</div>;
+  }
+
   return (
     <Layout>
-      {/* <Container
-      sx={{
-        backgroundColor: '#3A557F',
-        borderRadius: '12px',
-        minHeight: '100px',
-        margin: '32px auto',
-        width: '100%',
-        padding: '28px',
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    > */}
-      <Typography
-        variant="h5"
-        sx={{
-          paddingTop: '16px',
-        }}
-      >
-        New Wordpress Site
-      </Typography>
-
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          marginTop: '30px',
-        }}
-      >
+      <Tabs value={value} onChange={handleTabs}>
+        <Tab label="WordPress" />
+        <Tab label="App 2" />
+        <Tab label="App 3" />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <Typography
+          variant="h5"
+          sx={{
+            paddingTop: "16px",
+          }}
+        >
+          New Wordpress Site
+        </Typography>
         <Box
           sx={{
-            backgroundColor: '#fff',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '60px',
-            height: '60px',
-            opacity: '0.8',
+            display: "flex",
+            alignItems: "center",
+            marginTop: "30px",
           }}
         >
-          <Image src={'/images/wordpress.svg'} width={50} height={50} />
+          <Box
+            sx={{
+              backgroundColor: "#fff",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "60px",
+              height: "60px",
+              opacity: "0.8",
+            }}
+          >
+            <Image src={"/images/wordpress.svg"} width={50} height={50} />
+          </Box>
+          <Box
+            sx={{
+              marginLeft: "10px",
+            }}
+          >
+            <Typography variant="h6">WordPress</Typography>
+            <Typography variant="subtitle2">by SquareCloud</Typography>
+          </Box>
         </Box>
-        <Box
+        <Typography
+          variant="h6"
           sx={{
-            marginLeft: '10px',
+            paddingTop: "24px",
+            paddingBottom: "16px",
           }}
         >
-          <Typography variant="h6">WordPress</Typography>
-          <Typography variant="subtitle2">by SquareCloud</Typography>
+          Choose site name
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }} onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="wordpressname"
+            name="wordpressname"
+            autoFocus
+            variant="outlined"
+            sx={{
+              padding: "0",
+              marginBottom: "40px",
+              width: "40%",
+            }}
+          />
+          <Button
+            size="large"
+            type="submit"
+            fullWidth
+            variant="outlined"
+            disabled={isLoading}
+            sx={{
+              padding: "10px",
+              marginBottom: "40px",
+            }}
+          >
+            Create wordpress site
+          </Button>
         </Box>
-      </Box>
-      <Typography
-        variant="h6"
-        sx={{
-          paddingTop: '24px',
-          paddingBottom: '16px',
-        }}
-      >
-        Choose site name
-      </Typography>
-      <Box component="form" noValidate sx={{ mt: 1 }} onSubmit={handleSubmit}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="wordpressname"
-          name="wordpressname"
-          autoFocus
-          variant="outlined"
-          sx={{
-            padding: '0',
-            marginBottom: '40px',
-            width: '40%',
-          }}
-        />
-        <Button
-          size="large"
-          type="submit"
-          fullWidth
-          variant="outlined"
-          disabled={isLoading}
-          sx={{
-            padding: '10px',
-            marginBottom: '40px',
-          }}
-        >
-          Create wordpress site
-        </Button>
-      </Box>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        App 2 detail
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        App 3 detail
+      </TabPanel>
     </Layout>
   );
 }
