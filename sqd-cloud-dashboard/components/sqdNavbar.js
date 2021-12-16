5d196f077f9401c9d5293326b669e6ebd54a771c
Zoi Kousteni <zoikousteni@hotmail.com>
2021-12-16 14:11:14 +0200

    Navbar changes mainly

--- sqd-cloud-dashboard/components/sqdNavbar.js
+++ sqd-cloud-dashboard/components/sqdNavbar.js
@@ -1,20 +1,21 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import Image from 'next/image';
-import Badge from '@mui/material/Badge';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import Image from "next/image";
+import Badge from "@mui/material/Badge";
+import Button from "@mui/material/Button";
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-const pages = ['Overview', 'Sites', 'Apps', 'Databases', 'Settings'];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const pages = ["Overview", "Sites", "Apps", "Databases", "Settings"];
 
 const SqdNavbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -39,50 +40,58 @@ const SqdNavbar = () => {
     <AppBar
       position="static"
       sx={{
-        backgroundColor: 'transparent',
-        backgroundImage: 'none',
-        boxShadow: 'none',
-      }}>
-      <Container maxWidth="xl" sx={{ borderBottom: 1, marginBottom: '10px' }}>
-        <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
+        backgroundColor: "transparent",
+        backgroundImage: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Container maxWidth="xl" sx={{ borderBottom: 1, marginBottom: "10px" }}>
+        <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
           <Typography
             variant="h6"
             noWrap
             component="div"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'flex' },
-              display: 'flex',
-              alignItems: 'center',
-            }}>
-            <Image src="/images/sqdLogo.svg" alt="Squaredev Logo" width={40} height={40} />
-            <Typography variant="h6" sx={{ marginLeft: '10px' }}>
+              display: { xs: "flex", md: "flex" },
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              src="/images/sqdLogo.svg"
+              alt="Squaredev Logo"
+              width={40}
+              height={40}
+            />
+            <Typography variant="h6" sx={{ marginLeft: "10px" }}>
               Ana
             </Typography>
           </Typography>
 
-          <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center' }}>
-            <NotificationsIcon sx={{ marginRight: '10px' }} />
+          <Box sx={{ flexGrow: 0, display: "flex", alignItems: "center" }}>
+            <NotificationsIcon sx={{ marginRight: "10px" }} />
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}>
+              onClose={handleCloseUserMenu}
+            >
               {settings.map((setting) => (
                 <MenuItem key={setting} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">{setting}</Typography>
@@ -92,13 +101,24 @@ const SqdNavbar = () => {
           </Box>
         </Toolbar>
       </Container>
-      <Container maxWidth="xl" >
-        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } , justifyContent: "space-evenly" ,paddingBottom:"10px"}}>
-        {pages.map((page) => (
-          <Box key={page} sx={{display:"flex", alignSelf: "left"}}>
-            <Typography textAlign="left">{page}</Typography>
-          </Box>
-        ))}</Box>
+      <Container maxWidth="xl">
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "none", md: "flex" },
+            justifyContent: "space-evenly",
+            paddingBottom: "10px",
+          }}
+        >
+          {pages.map((page) => (
+            <Box key={page} sx={{ display: "flex", alignSelf: "left" }}>
+              <Button variant="text">
+                {" "}
+                <Typography textAlign="left" >{page}</Typography>
+              </Button>
+            </Box>
+          ))}
+        </Box>
       </Container>
     </AppBar>
   );
--- sqd-cloud-dashboard/pages/dashboard.js
+++ sqd-cloud-dashboard/pages/dashboard.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Container, Typography } from "@mui/material";
-import SqdNavbar from "../components/sqdNavbar";
+import { Layout } from "/components/layout.js";
 export default function Dashboard() {
   return (
     <>
-      <SqdNavbar />
-      <Typography variant="h4">Welcome back</Typography>
-      <Container
-        sx={{
-          backgroundColor: "#3A557F",
-          borderRadius: "12px",
-          minHeight: "100px",
-          margin: "32px auto",
-          width: "100%",
-          padding: "28px",
-        }}
-      >
-        Dashboard
-      </Container>
+      <Layout>
+        <Typography variant="h4">Welcome back</Typography>
+        <Container
+          sx={{
+            backgroundColor: "#3A557F",
+            borderRadius: "12px",
+            minHeight: "100px",
+            margin: "32px auto",
+            width: "100%",
+            padding: "28px",
+          }}
+        >
+          Dashboard
+        </Container>
+      </Layout>
     </>
   );
 }
