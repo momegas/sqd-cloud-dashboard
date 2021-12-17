8f24935de46c2459096b0cfaa075b6a645f856f6
sotberd <sotirios.berdes@squaredev.io>
2021-12-17 15:29:42 +0200

    add postgresql connection string

--- sqd-cloud-dashboard/components/DatabaseContainer.js
+++ sqd-cloud-dashboard/components/DatabaseContainer.js
@@ -1,7 +1,7 @@
-import { Box, Grid, IconButton, Typography } from "@mui/material";
-import Image from "next/image";
-import React from "react";
-import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Grid, IconButton, Typography } from '@mui/material';
+import Image from 'next/image';
+import React from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function DatabaseContainer({ db, onDelete }) {
   const stackName = db.stackName;
@@ -26,27 +26,27 @@ export default function DatabaseContainer({ db, onDelete }) {
     >
       <Box
         sx={{
-          backgroundColor: "#09172A",
-          borderRadius: "8px",
-          minHeight: "100px",
-          padding: "16px",
-          border: "1px solid #A4ACC4",
-          display: "flex",
-          flexDirection: "column",
-          width: "100%",
+          backgroundColor: '#09172A',
+          borderRadius: '8px',
+          minHeight: '100px',
+          padding: '16px',
+          border: '1px solid #A4ACC4',
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
         }}
       >
         <Box
           sx={{
-            display: "flex",
-            alignItems: "center",
+            display: 'flex',
+            alignItems: 'center',
           }}
         >
           <Image
             src={
-              stackType === "postgres"
-                ? "/images/postgresql.svg"
-                : "/images/mongo.svg"
+              stackType === 'postgres'
+                ? '/images/postgresql.svg'
+                : '/images/mongo.svg'
             }
             width={30}
             height={30}
@@ -54,8 +54,8 @@ export default function DatabaseContainer({ db, onDelete }) {
           <Typography
             variant="body"
             sx={{
-              fontSize: "20px",
-              marginLeft: "10px",
+              fontSize: '20px',
+              marginLeft: '10px',
             }}
           >
             {stackName}
@@ -64,28 +64,28 @@ export default function DatabaseContainer({ db, onDelete }) {
         <Typography
           variant="subtitle2"
           sx={{
-            paddingTop: "10px",
-            paddingBottom: "6px",
-            opacity: "0.8",
+            paddingTop: '10px',
+            paddingBottom: '6px',
+            opacity: '0.8',
           }}
         >
-          {stackType === "postgres"
-            ? ""
+          {stackType === 'postgres'
+            ? `postgresql://${databaseUser?.value}:${databasepass?.value}@cluster1.squaredev.io:${appPort}`
             : `mongodb://${databaseName?.value}:${databasepass?.value}@cluster1.squaredev.io:${appPort}`}
         </Typography>
         <IconButton
           aria-label="delete"
           onClick={() => onDelete(stackName)}
           sx={{
-            fontSize: "20px",
-            opacity: "0.6",
-            cursor: "pointer",
-            marginLeft: "auto",
+            fontSize: '20px',
+            opacity: '0.6',
+            cursor: 'pointer',
+            marginLeft: 'auto',
           }}
         >
           <DeleteIcon
             sx={{
-              fontSize: "20px",
+              fontSize: '20px',
             }}
           />
         </IconButton>
